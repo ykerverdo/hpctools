e5a39cfe8c376d46673278b4bd5219884d665fd3
Yann <yann.kerverdo@gmail.com>
2025-09-29 19:03:34 +0200

    .h + commentary

--- dgesv.c
+++ dgesv.c
@@ -3,19 +3,20 @@
 #include <math.h>
 #include <stdio.h>
 
+// Function to swap two rows in a matrix
 int swap_rows(double *matrix, int row1, int row2, int n) {
   for (int j = 0; j < n; j++) {
     double temp = matrix[row1 * n + j];
     matrix[row1 * n + j] = matrix[row2 * n + j];
     matrix[row2 * n + j] = temp;
   }
-  return 0;
 }
 
 int my_dgesv(int n, int nrhs, double *a, double *b)
 {
   for (int i=0;i<n;i++) {
 
+    // Search for maximum in this column
     int swap_row = i;
     double max_val = fabs(a[i*n + i]);
     for (int j = i+1; j < n; j++) {
@@ -26,12 +27,14 @@ int my_dgesv(int n, int nrhs, double *a, double *b)
       }
     }
 
+    // Swap maximum row with current row
     if (swap_row != i) {
       swap_rows(a, i, swap_row, n);
       swap_rows(b, i, swap_row, nrhs);
     }
 
     double pivot = a[i*n+i];
+    // Transform to upper triangular matrix
     for (int j=i+1;j<n;j++) {
       double factor = a[j*n+i]/pivot;
       a[j*n+i] = 0.0;
@@ -44,6 +47,7 @@ int my_dgesv(int n, int nrhs, double *a, double *b)
     }
   }
 
+  // Back substitution
   for (int i=0;i<nrhs;i++) {
     for (int j=n-1;j>=0;j--) {
       for (int k=j+1;k<n;k++) {
--- dgesv.h
+++ dgesv.h
@@ -1,3 +1,2 @@
 int my_dgesv(int n, int nrhs, double *a, double *b /* add/change the arguments according to your implementation needs */);
 int swap_rows(double *matrix, int row1, int row2, int n);
-int is_too_small(double x);
